4839edfec0b167dc52cf922dea4b2b67c831cd41
Richard Ward <johndoe6345789@gmail.com>
2025-12-11 15:00:59 +0000

    test

--- test-tooling/jest.config.cjs
+++ test-tooling/jest.config.cjs
@@ -1,14 +1,19 @@
+const path = require("path");
+const nodeModulesDir = path.join(__dirname, "node_modules");
+
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
   rootDir: ".",
   testMatch: ["**/tests/**/*.test.ts?(x)"],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"],
+  moduleDirectories: ["node_modules", nodeModulesDir],
+  modulePaths: [nodeModulesDir],
   moduleNameMapper: {
     "^@mui/material/(.*)$": "<rootDir>/node_modules/@mui/material/$1",
     "^@mui/material$": "<rootDir>/node_modules/@mui/material",
-    "^react/jsx-runtime$": "<rootDir>/node_modules/react/jsx-runtime.js",
-    "^react/jsx-dev-runtime$": "<rootDir>/node_modules/react/jsx-dev-runtime.js"
+    "^react/jsx-runtime$": `${nodeModulesDir}/react/jsx-runtime.js`,
+    "^react/jsx-dev-runtime$": `${nodeModulesDir}/react/jsx-dev-runtime.js`
   },
   globals: {
     "ts-jest": {
